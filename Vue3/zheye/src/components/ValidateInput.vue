24dcedf2d2fe4592b71414e3f3bc7ef95132b8f0
zhangzhitao <1084024036@qq.com>
2022-04-26 09:21:44 +0800

    修改input事件类型

--- Vue3/zheye/src/components/ValidateInput.vue
+++ Vue3/zheye/src/components/ValidateInput.vue
@@ -54,7 +54,7 @@ export default defineComponent({
 				inputRef.error = !allPassed;
 			}
 		}
-		const updateValue = (e: KeyboardEvent) => {
+		const updateValue = (e: Event) => {
 			const targetValue = (e.target as HTMLInputElement).value;
 			inputRef.value = targetValue;
 			context.emit('update:modelValue', targetValue);
